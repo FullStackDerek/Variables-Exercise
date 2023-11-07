873f798fe3241964ff7e5ccf279a3e8d64b8b4af
derek madrigal <derekbmadrigal@gmail.com>
2023-11-07 15:01:50 -0800

    Declare and Initialize Complete

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declare and initialize variables
+string name = "Derek";
+int age = 33;
+char middleInitial = 'B';
+bool isOver18 = true;
+double currentTemp = 69.9;
+decimal currentPrice = 420.69m;
+
+Console.WriteLine($"Name:{name}");
+Console.WriteLine($"Age:{age}");
+Console.WriteLine($"Middle Initial:{middleInitial}");
+Console.WriteLine($"Is Over 18:{isOver18}");
+Console.WriteLine($"Current Temp:{currentTemp}");
+Console.WriteLine($"Current Price:{currentPrice}");
